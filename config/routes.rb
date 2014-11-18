8fb878bc0a79b4c78400fabad1e367ed2d5144a6
Farhan1013 <Farhan1013@gmail.com>
2014-11-18 14:47:53 -0800

    Fix routes.rb root path conflict

--- config/routes.rb
+++ config/routes.rb
@@ -5,24 +5,6 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-
-
-  root 'home#index'
-
-  root 'yousuck#index'
-
-  roo 'planes#index'
-
-
-
-
-
-
-
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
   
 
   root 'home#index'
